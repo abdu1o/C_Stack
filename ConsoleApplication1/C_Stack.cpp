161558afa90f53e323ff2026a6a3cb4810bd23f0
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-12 15:21:06 +0200

    +StringToInt function

--- ConsoleApplication1/C_Stack.cpp
+++ ConsoleApplication1/C_Stack.cpp
@@ -1,10 +1,68 @@
 ﻿#include <iostream>
 #include <exception>
+#include <string>
 using namespace std;
 #include "Class.h"
 
+int StringToInt(const string& str)
+{
+    int result = 0;
+    bool negative = false;
+    int i = 0;
+
+    if (str[0] == '-') 
+    {
+        negative = true;
+        i++;
+    }
+
+    for (int i = 0; i < str.length(); i++) 
+    {
+        if (str[i] >= '0' && str[i] <= '9') 
+        {
+            int digit = str[i] - '0';
+
+            if (negative) 
+            {
+                digit = -digit;
+            }
+
+            if (result > 2147483647 || (result == 2147483647 && digit > 7))
+            {
+                throw ("Int is out bounds");
+            }
+            if (result < -2147483647 || (result == -2147483647 && digit < -8))
+            {
+                throw ("Int is out bounds");
+            }
+            result = result * 10 + digit;
+        }
+        else 
+        {
+            throw ("Wrong string input");
+        }
+    }
+    return result;
+}
+
 int main() 
 {
+    //1 task
+    string str;
+    cout << "Enter an string of numbers: ";
+    cin >> str;
+    try 
+    {
+        int result = StringToInt(str);
+        cout << "Result: " << result << endl;
+    }
+    catch (const exception& ex) 
+    {
+        cerr << "Error: " << ex.what() << endl;
+    }
+
+
+    //2 task
     try 
     {
         Stack stack(5);
